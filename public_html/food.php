4384fcafb21a4f194c0f3da8f17c66f2aff95d10
Erix2323 <erix2323@mail.ru>
2016-04-06 10:40:43 +0300

    first_commit

---
+++ public_html/food.php
@@ -0,0 +1,13 @@
+
+<?php
+$link = mysql_connect('localhost', 'root', 'root1');
+if (!$link) {
+    die('Could not connect: ' . mysql_error());
+}
+echo 'Connected successfully';
+
+
+
+mysql_close($link);
+
+
